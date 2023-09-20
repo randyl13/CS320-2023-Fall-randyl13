f88c038aa2afa98f384104e67aa1c6e6d6485d72
randyl13 <rdleemit@gmail.com>
2023-09-20 15:48:49 -0400

    cleaned code up

--- assigns/assign1/MySolution/assign1-1.ml
+++ assigns/assign1/MySolution/assign1-1.ml
@@ -11,15 +11,6 @@ let rec intrev n acc: int =
 let intrev10(n: int): int = 
   intrev n 0
 
-let () =
-assert(intrev10(1234) = 4321)
-let () =
-assert(intrev10(1020304) = 4030201)
-let () =
-assert(intrev10(intrev10(13579)) = 13579)
-(* ****** ****** *)
-let () =
-print_string("Assign1-1-test passed!\n")
 
 
 
