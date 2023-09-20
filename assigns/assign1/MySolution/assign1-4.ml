f824592359b984a63e4a41660d591ce217dc411f
randyl13 <rdleemit@gmail.com>
2023-09-20 15:47:35 -0400

    worked on assign1-1 and assign1-2

--- assigns/assign1/MySolution/assign1-4.ml
+++ assigns/assign1/MySolution/assign1-4.ml
@@ -1,15 +1,1 @@
 #use "./../../../classlib/OCaml/MyOcaml.ml";;
-let i1 = 0;;
-let i2 = 0;;
-
-
-let lowercase_letters_work work_function =
-  for c = Char.code 'a' to Char.code 'z' do
-    work_function (Char.chr c)
-    
-  for c = Char.code 'a' to Char.code 'z' do
-    i1 + 1
-  done
-;;
-
-let lowercase_alphabet = string_make_fwork lowercase_letters_work
