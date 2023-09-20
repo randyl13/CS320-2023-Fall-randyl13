94f98308a90d34f8a46aa55e8235830fc76f9cb0
randyl13 <rdleemit@gmail.com>
2023-09-20 16:30:21 -0400

    worked on assign1-3

---
+++ assigns/assign1/MySolution/assign1-3.ml
@@ -0,0 +1,27 @@
+#use "./../../../classlib/OCaml/MyOcaml.ml";;
+
+let rec check_middle left right cs index = 
+if index = right then false
+else if (string_get_at cs index > string_get_at cs right) && (string_get_at cs right > string_get_at cs left )
+    then true
+else check_middle left right cs (index + 1)
+
+let rec indexLoop left right cs =
+  if right < string_length cs then
+    if string_get_at cs left < string_get_at cs right then
+      if check_middle left right cs (left + 1) then 
+        false
+      else 
+        indexLoop left (right + 1) cs
+    else
+      indexLoop left (right + 1) cs
+  else true
+
+let rec myLoop left cs = 
+  if left <= (string_length cs) - 3 then
+    indexLoop left (left + 2) cs
+  else
+    myLoop (left + 1) cs
+
+let string_avoid_132(cs: string): bool = 
+    indexLoop 0 2 cs
